4e825084ea204bfa8c30eb773f8447fbffd5ff38
geekelo <efueliht12@gmail.com>
2025-03-11 22:57:38 +0100

    Add cors

--- db/migrate/20250311111716_drop_clacbt_users_and_exams.rb
+++
@@ -1,8 +0,0 @@
-class DropClacbtUsersAndExams < ActiveRecord::Migration[7.1]
-  def change
-    drop_table :cla_cbt_answers
-    drop_table :cla_cbt_questions
-    drop_table :cla_cbt_candidates
-    drop_table :cla_cbt_exams
-  end
-end
