fc6d3ff3476ebb8e8d5d812ceaeb9e386a1398a8
geekelo <efueliht12@gmail.com>
2025-03-11 11:06:24 +0100

    Set up auth

--- Gemfile
+++ Gemfile
@@ -9,7 +9,7 @@ gem "rails", "~> 7.1.5", ">= 7.1.5.1"
 gem "sprockets-rails"
 
 # Use sqlite3 as the database for Active Record
-gem "sqlite3", ">= 1.4"
+# gem "sqlite3", ">= 1.4"
 
 # Use the Puma web server [https://github.com/puma/puma]
 gem "puma", ">= 5.0"
@@ -26,6 +26,7 @@ gem "stimulus-rails"
 # Build JSON APIs with ease [https://github.com/rails/jbuilder]
 gem "jbuilder"
 
+ gem "pg"
 # Use Redis adapter to run Action Cable in production
 # gem "redis", ">= 4.0.1"
 
@@ -65,3 +66,8 @@ group :test do
   gem "capybara"
   gem "selenium-webdriver"
 end
+
+gem 'active_model_serializers'
+gem 'bcrypt'
+gem 'jwt'
+gem 'rack-cors'
---
+++ app/controllers/api/v1/authentication_controller.rb
@@ -0,0 +1,24 @@
+
+module Api
+  module V1
+    class AuthenticationController < ApplicationController
+      def create
+        user = ClacbtUser.find_by(email: login_params[:email])
+        if user&.authenticate(login_params[:password])
+          render json: {
+            message: 'User is logged in',
+            user: ClacbtUserSerializer.new(user),
+            token: JsonWebToken.encode({ sub: user.id })
+          }, status: :ok
+        else
+          render json: { error: 'Invalid credentials' }, status: :unauthorized
+        end
+      end
+      private
+
+      def login_params
+        params.require(:clacbt_user).permit(:email, :password)
+      end
+    end
+  end
+end
