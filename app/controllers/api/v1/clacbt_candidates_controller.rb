655da8683f56ba5047404fecbc1e9a5c02d03f46
geekelo <efueliht12@gmail.com>
2025-03-13 16:57:32 +0100

    Raise exception for update and check candidate

--- app/controllers/api/v1/clacbt_candidates_controller.rb
+++ app/controllers/api/v1/clacbt_candidates_controller.rb
@@ -1,5 +1,6 @@
 class Api::V1::ClacbtCandidatesController < ApplicationController
-  before_action :authenticate_token!, except: [:check_candidate, :update]
+  before_action :authenticate_token!, except: [:check_candidate]
+  skip_before_action :authenticate_token!, only: :update
   before_action :set_exam, only: [:index, :create]
   before_action :set_candidate, only: [:show, :update, :destroy]
 
