46a6e0a22cc511b6b1759ca950732265af57ed35
geekelo <efueliht12@gmail.com>
2025-03-13 19:33:36 +0100

    Improve code

--- app/controllers/api/v1/clacbt_candidates_controller.rb
+++ app/controllers/api/v1/clacbt_candidates_controller.rb
@@ -1,9 +1,8 @@
 class Api::V1::ClacbtCandidatesController < ApplicationController  
-  # before_action :authenticate_token!, except: [:check_candidate, :update] 
-  # skip_before_action :authenticate_token!, only: [:check_candidate, :update]
+  before_action :authenticate_token!, except: [:check_candidate, :update] 
   before_action :set_exam, only: [:index, :create]
-  # before_action :set_candidate, only: [:show, :update, :destroy]
-  # before_action :set_candidate, except: [:check_candidate]  
+  before_action :set_candidate, only: [:show, :update, :destroy]
+  before_action :set_candidate, except: [:check_candidate]  
 
   # Fetch candidates for a specific exam (Only exam owner can view)
   def index
@@ -39,36 +38,6 @@ class Api::V1::ClacbtCandidatesController < ApplicationController
   def destroy
     @candidate.destroy
     render json: { message: "Candidate deleted successfully" }, status: :ok
-  end
-
-  # Publicly display a candidate
-  def check_candidate
-    ActiveRecord::Base.connection.clear_cache! # Clear cache to ensure latest data is fetched
-  
-    # Ensure exam_code is provided
-    unless params[:exam_code].present?
-      return render json: { error: "Exam code is required" }, status: :bad_request
-    end
-  
-    # Find exam safely
-    exam = ClacbtExam.find_by(exam_code: params[:exam_code])
-    return render json: { error: "Exam not found" }, status: :not_found unless exam
-  
-    # Find candidate (ignoring case for email)
-    candidate = exam.clacbt_candidates.find_by("LOWER(email) = ?", params[:email].to_s.downcase)
-  
-    if candidate&.score.nil?
-      render json: { 
-        message: "Candidate authorized", 
-        candidate: {
-          id: candidate.id,
-          email: candidate.email, 
-          exam_code: exam.exam_code 
-        } 
-      }, status: :ok
-    else
-      render json: { error: "Unauthorized candidate" }, status: :unauthorized
-    end
   end
   
   private
