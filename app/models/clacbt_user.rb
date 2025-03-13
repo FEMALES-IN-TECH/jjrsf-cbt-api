9059567360d5d1fad745d7bfade920d96203e8c2
geekelo <efueliht12@gmail.com>
2025-03-13 11:25:02 +0100

    Add missing association betweebn user and exams

--- app/models/clacbt_user.rb
+++ app/models/clacbt_user.rb
@@ -1,5 +1,6 @@
 class ClacbtUser < ApplicationRecord
     has_secure_password
+    has_many :clacbt_exams
   
     validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
     validates :password, presence: true, length: { minimum: 6 }, if: :password_digest_changed?
