fe52af0b14b56d39813f49fc1906623d56f1579e
geekelo <efueliht12@gmail.com>
2025-03-13 10:35:46 +0100

    Fix route to fetch exams

--- config/routes.rb
+++ config/routes.rb
@@ -12,8 +12,11 @@ Rails.application.routes.draw do
       resources :clacbt_exams, except: [:new, :edit] do
         resources :clacbt_questions, except: [:new, :edit]  # Nested under exams
         resources :clacbt_candidates, except: [:new, :edit] # Nested under exams
+      end
+
+      resources :clacbt_exams, only: [:index, :show, :create, :update, :destroy] do
         collection do
-          get 'display_exam'
+          get 'display_exam'  # Route for displaying an exam without authentication
         end
       end
 
