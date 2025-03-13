83d8ca5192c83949b09eb1a99e157888697558fa
geekelo <efueliht12@gmail.com>
2025-03-13 13:50:47 +0100

    Remove auth for updating candidate

--- app/controllers/api/v1/clacbt_candidates_controller.rb
+++ app/controllers/api/v1/clacbt_candidates_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::ClacbtCandidatesController < ApplicationController
-  before_action :authenticate_token!, except: [:check_candidate]
+  before_action :authenticate_token!, except: [:check_candidate, :update]
   before_action :set_exam, only: [:index, :create]
   before_action :set_candidate, only: [:show, :update, :destroy]
 
