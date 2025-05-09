84e340cf103abfeb0e443fe04d7b034d1c50653f
Eloghene <eloghene@bananacrystal.com>
2025-05-09 12:43:13 +0100

    Add url in cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-      origins ['http://localhost:3000', 'https://cbt.jjrsf.org', 'https://cbt-admin.jjrsf.org', 'https://jjrsf-cbt-admin.vercel.app/login'] # Add your frontend origin(s)
+      origins ['http://localhost:3000', 'https://cbt.jjrsf.org', 'https://cbt-admin.jjrsf.org', 'https://jjrsf-cbt-admin.vercel.app'] # Add your frontend origin(s)
   
       resource '/api/*',
         headers: :any,
